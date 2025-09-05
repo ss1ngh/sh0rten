cb5dbdde9c3d1bad90d3930490f2bd5681e66a19
ss1ngh <singhshreyansh2354@gmail.com>
2025-09-05 17:24:51 +0530

    feat : return existing shortUrl if url is already shortened instead of regenerating a new one to reduce duplicacy

--- backend/src/controller/urlController.js
+++ backend/src/controller/urlController.js
@@ -2,26 +2,37 @@ import URL from "../models/url.js";
 import { nanoid } from "nanoid";
 
 export const createShortUrl = async (req, res) => {
-    try {
-        const { url } = req.body;
+  try {
+    const { url } = req.body;
 
-        if (!url) {
-            return res.status(400).json({ message: "URL is required" });
-        }
-
-        const shortID = nanoid(8);
-
-        await URL.create({
-            shortId: shortID,
-            fullUrl : url,
-        })
-
-        res.status(201).json({
-            shortUrl: `${req.protocol}://${req.get("host")}/${shortID}`,
-            fullUrl: url
-        });
-
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+    if (!url) {
+      return res.status(400).json({ message: "URL is required" });
     }
+
+    //check if the shortened URL already exists in the DB
+    let existingUrl = await URL.findOne({ fullUrl: url });
+    if (existingUrl) {
+      return res.status(200).json({
+        shortUrl: `${req.protocol}://${req.get("host")}/${existingUrl.shortId}`,
+        fullUrl: existingUrl.fullUrl
+      });
+    }
+
+    //otherwise create a new shortened URL
+    const shortID = nanoid(8);
+
+    const newUrl = await URL.create({
+      shortId: shortID,
+      fullUrl: url,
+      totalClicks: 0
+    });
+
+    res.status(201).json({
+      shortUrl: `${req.protocol}://${req.get("host")}/${newUrl.shortId}`,
+      fullUrl: newUrl.fullUrl
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
