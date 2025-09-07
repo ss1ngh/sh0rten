4a645341a875ee999e95b491a55809bf1e48950f
ss1ngh <singhshreyansh2354@gmail.com>
2025-09-07 15:12:45 +0530

    final changes

--- backend/app.js
+++ backend/app.js
@@ -21,10 +21,4 @@ app.use(express.json());
 app.use('/api', urlRoutes);
 app.get('/:shortId', redirectFromShortUrl);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-
 export default app;
