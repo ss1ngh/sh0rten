9e0ad64d68aaa0ab03d08dfd3273ed1fa8770980
ss1ngh <singhshreyansh2354@gmail.com>
2025-09-09 03:48:02 +0530

    feat: added speed insights

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import NotFound from "./pages/NotFound";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+
 
 const queryClient = new QueryClient();
 
@@ -21,6 +23,7 @@ const App = () => (
         </Routes>
       </BrowserRouter>
       <Analytics />
+      <SpeedInsights/>
     </TooltipProvider>
   </QueryClientProvider>
 );
